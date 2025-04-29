162f439883f68f5091bf54b6949a1e22dfa90f60
הנסב רבקה < @mbjcomp.local>
2025-04-29 20:04:56 +0300

    new

--- Server/Program.cs
+++ Server/Program.cs
@@ -19,8 +19,20 @@ builder.Services.AddSingleton<DatabaseManager>();
 builder.Services.AddSingleton<IBL, BLManager>();
 builder.Services.AddControllers();
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 var app = builder.Build();
+app.UseCors("AllowAllOrigins");
+
+
+
+
+
 app.MapControllers();
 app.Run();
