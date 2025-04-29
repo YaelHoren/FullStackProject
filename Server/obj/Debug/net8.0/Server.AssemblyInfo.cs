2d312cd16c3a57316afb42f0572ba26e0b38492c
הנסב רבקה < @mbjcomp.local>
2025-04-29 20:32:55 +0300

    new2

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281563adfa2036d77730bd7ced60b069824d70f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162f439883f68f5091bf54b6949a1e22dfa90f60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
